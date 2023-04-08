73c2020a88506d3a7750964dc6a4c51a56270dd9
Evangelos (Vangelis) Katsadouros <katsadouros.v@gmail.com>
2023-04-08 20:24:19 +0300

    Update requirements.txt
    Change tweepy with twython.

---
+++ tweets_cascade.py
@@ -0,0 +1,5 @@
+# Author: Evangelos Katsadouros
+# Email: katsadouros.v@gmail.com
+# Description: This script retrieves a root tweet and
+# its retweets from Twitter and generates a cascade view of the tweet tree.
+# Python Version: 3.10
